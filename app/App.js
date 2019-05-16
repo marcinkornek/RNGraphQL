c4000b4227c4f2135cfef29ac25e66ec669711f7
Marcin Kornek <marcin.kornek@netguru.pl>
2019-05-16 16:33:01 +0200

    Add ApolloProvider

--- app/App.js
+++ app/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
 import RootNavigator from './routes/RootNavigator'
+
+const client = new ApolloClient({ uri: 'http://localhost:4444' })
 
 function App() {
   return (
-    <RootNavigator />
+    <ApolloProvider client={client}>
+      <RootNavigator />
+    </ApolloProvider>
   )
 }
 

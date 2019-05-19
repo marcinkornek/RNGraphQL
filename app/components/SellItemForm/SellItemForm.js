388094986c40fd122c029f6909537df977d7e06d
Marcin Kornek <marcin.kornek@netguru.pl>
2019-05-19 13:31:42 +0200

    Style SellItemForm

--- app/components/SellItemForm/SellItemForm.js
+++ app/components/SellItemForm/SellItemForm.js
@@ -1,8 +1,30 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
 import Error from '../ErrorMessage/ErrorMessage';
+
+const styles = StyleSheet.create({
+  form: {
+    padding: 10,
+  },
+  field: {
+    marginVertical: 10
+  },
+  label: {
+    marginBottom: 5,
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'gray',
+  },
+  input: {
+    height: 40,
+    paddingHorizontal: 10,
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: 'gray',
+    borderRadius: 10,
+  },
+})
 
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -41,41 +63,49 @@ function CreateItem() {
   return (
     <Mutation mutation={CREATE_ITEM_MUTATION} variables={value}>
       {(createItem, { loading, error }) => (
-        <View>
+        <View style={styles.form}>
           <Error error={error} />
 
-          <Text>Title</Text>
-          <TextInput
-            placeholder="Title"
-            value={value.title}
-            onChangeText={value => this.handleChange(value, 'title')}
-          />
-
-          <Text>Price</Text>
-          <TextInput
-            placeholder="Price"
-            keyboardType='numeric'
-            value={value.price}
-            onChangeText={value => this.handleChange(value, 'price')}
-          />
-
-            <Text>Description</Text>
+          <View style={styles.field}>
+            <Text style={styles.label}>Title</Text>
             <TextInput
+              style={styles.input}
+              placeholder="Title"
+              value={value.title}
+              onChangeText={value => this.handleChange(value, 'title')}
+            />
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.label}>Price</Text>
+            <TextInput
+              style={styles.input}
+              placeholder="Price"
+              keyboardType='numeric'
+              value={value.price}
+              onChangeText={value => this.handleChange(value, 'price')}
+            />
+          </View>
+
+          <View style={styles.field}>
+            <Text style={styles.label}>Description</Text>
+            <TextInput
+              style={styles.input}
               placeholder="Enter A Description"
               value={value.description}
               onChangeText={value => this.handleChange(value, 'description')}
               multiline
             />
+          </View>
 
-            <Button
-              title="Submit"
-              onPress={async () => {
-                // call the mutation
-                const res = await createItem();
-                // change them to the single item page
-                console.log('created', res);
-              }}
-            />
+          <Button
+            title="Submit"
+            onPress={async () => {
+              // call the mutation
+              const res = await createItem();
+              console.log('created', res);
+            }}
+          />
         </View>
       )}
     </Mutation>
